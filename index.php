cb83e7c1eccc74d4e1e31f7cd20133cf6a576e8a
AfterNath <78166954+AfterNath@users.noreply.github.com>
2022-04-28 20:38:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>AfterNath</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap" rel="stylesheet">
+    <link href="css/terminal.css?<?php echo time();?>" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="/imgs/favicon.png">
+  </head>
+  <body>
+    <div id="terminal">
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
+    <script src="js/terminal.js?<?php echo time();?>"></script>
+    <script src="js/defaultcommands.js?<?php echo time();?>"></script>
+  </body>
+</html>
